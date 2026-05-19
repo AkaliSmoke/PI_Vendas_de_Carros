12888193ff877ee6af9d2a89ae63aba185dd3703
Kaio <kaiomascarenhas241@gmail.com>
2026-05-19 20:18:19 -0300

    corrigindo renomeação dos arquivos

--- 03_projeto_intercador/cliente-salvar.php
+++ 03_projeto_intercador/cliente-salvar.php
@@ -1,33 +1,12 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>cliente</title>
-</head>
-<body>
-    <h1>cliente</h1>
-    <form action="cliente-cadastrar.php" method="post">
-        <label>cliente:</label>
-        <input type="text" name="veiculos"><br>
-        <label>clientes:</label>
-        <input type="text" name="clientes"><br>
-        <button type="submit">Acessar</button>
-        <button type="reset">Limpar</button>
-    </form>
- 
-    <?php
-    echo $_GET['mensagem'] ?? "";
-    ?>
-    <a href="cliente-cadastrar.php">cliente-cadastrar</a>
+<?php
+$nome = $_POST("nome");
+$telefone = $_POST("telefone");
+$idade = $_POST("idade");
+$estado = $_POST("estados");
+$cidade = $_POST("cidade");
 
-    <?php
-        $arquivo = fopen("album_db.txt", "r");
-        while(!feof($arquivo)){
-                $linha = fgets($arquivo);
-                echo "$linha<br>";
-        }
-        fclose($arquivo);
-        ?>
-</body>
-</html>
+$arquivo = fopen("cliente_db.txt", "a");
+fwrite($arquivo, "$nome \t $telefone  \t $idade \t $estado \t $cidade \n");
+fclose($arquivo);
+header("location: album-cadastrar.php")
+?>
