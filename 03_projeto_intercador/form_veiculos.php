a742fd61b29a55008e746d250562f58a14e67b87
leandropro12 <227642795+lelepepe50-droid@users.noreply.github.com>
2026-05-19 19:15:12 -0300

    Upateprojetos

---
+++ 03_projeto_intercador/form_veiculos.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+</head>
+<body>
+    <h1>Login</h1>
+    <form action="veiculos.php" method="post">
+        <label>veiculos:</label>
+        <input type="text" name="veiculos"><br>
+        <label>clientes:</label>
+        <input type="text" name="clientes"><br>
+        <button type="submit">Acessar</button>
+        <button type="reset">Limpar</button>
+    </form>
+ 
+    <?php
+    echo $_GET['mensagem'] ?? "";
+    ?>
+
+    <a href="cadastra_veiculos.php">cadastra_veiculos</a>
+</body>
+</html>
---
+++ 03_projeto_intercador/veiculos.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>cadastrar</title>
+</head>
+<body>
+   
+    <h1>Cadastrar</h1>
+    <form action="cadastrar.php" method="post">
+    <label>Marca:</label>
+    <input type="text"name="marca"><br>
+    <label>Modelo:</label>
+      <input type="text"name="modelo"><br>
+    <label>Ano:</label><br>
+      <input type="number"ano="ano"><br>
+    <label>quilometragem:</label><br>
+      <input type="number"quilometragem="quilometragem"><br>
+    <label>Combustivel:</label>
+    <input type="text" name="Combustivo"><br>
+    <label>Cor:</label>
+    <input type="text" name="cor"><br>
+    <label>Preço:</label>
+    <input type="number" name="preço"><br>
+
+    <a href="form_veiculos.php">$veiculos</a>
