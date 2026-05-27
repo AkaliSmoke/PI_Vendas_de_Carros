4b958b27a575d6d4fb30cd1a5ba165a764ddeeb7
leandropro12 <227642795+lelepepe50-droid@users.noreply.github.com>
2026-05-27 14:07:58 -0300

    update

--- 03_projeto_intercador/index.php
+++ 03_projeto_intercador/index.php
@@ -30,8 +30,8 @@
    <div class="card" style="width: 18rem;">
   <img src="/img/img/polo.jpg" class="card-img-top" alt="...">
   <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+    <h5 class="card-title"></h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
     <a href="#" class="btn btn-primary">Go somewhere</a>
   </div>
 </div>
