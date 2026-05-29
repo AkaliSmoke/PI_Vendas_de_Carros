7834a7802b4665f5f6e0e6586bcf5192b185b420
leandropro12 <227642795+lelepepe50-droid@users.noreply.github.com>
2026-05-28 21:27:20 -0300

    update index.php

--- 03_projeto_intercador/index.php
+++ 03_projeto_intercador/index.php
@@ -29,8 +29,6 @@
         </div>
       </div>
     </div>
-
-
     <div class="col">Column
         <div class="card" style="width: 18rem;">
            <img src="/img/img/camaro.jpg" class="card-img-top" alt="200">
@@ -41,8 +39,6 @@
          </div>
            </div>
     </div>
-
-<<<<<<< HEAD
   <div class="col">Column    
     <div class="card" style="width: 18rem;">
         <img src="/img/img/ford mustang gt.jpg" class="card-img-top" alt="200">
@@ -52,45 +48,6 @@
           <a href="#" class="btn btn-primary">ford mustang gt</a>
       </div>
    </div>
-=======
-   <div class="card" style="width: 18rem;">
-  <img src="/img/img/dodger challeger.jpg" class="card-img-top" alt="200">
-  <div class="card-body">
-    <h5 class="card-title">Dodge</h5>
-    <p class="card-text">O Dodge Challenger nasceu em 1970 para rivalizar no segmento dos pony cars. Ao longo das décadas, o modelo evoluiu de um clássico motor V8 para um cupê compacto na década de 1980, até retornar às suas raízes como um muscle car moderno e de alto desempenho na era contemporânea.</p>
-    <a href="#" class="btn btn-primary">Dodge challenger 2015</a>
->>>>>>> 125858193464ad7ac89f42dafa9fb011d014ca8f
-  </div>
-
-<div class="card" style="width: 18rem;">
-  <img src="/img/img/Chevrolet Camaro Wallpaper.jpg" class="card-img-top" alt="200">
-  <div class="card-body">
-    <h5 class="card-title">Chevrolet</h5>
-    <p class="card-text">O Chevrolet Camaro SS (Super Sport) nasceu em 1967 como a resposta da GM ao Ford Mustang. Famoso pelo potente motor V8 e visual agressivo, o modelo evoluiu ao longo de seis gerações, transformando-se em um ícone de desempenho tanto nas pistas quanto na cultura pop.</p>
-    <a href="#" class="btn btn-primary">Camaro SS</a>
-  </div>
-</div>
-
-<div class="card" style="width: 18rem;">
-  <img src="/img/img/BMW M3" class="card-img-top" alt="200">
-  <div class="card-body">
-    <h5 class="card-title">BMW</h5>
-    <p class="card-text">O BMW M3 nasceu em 1986 com um propósito claro: homologar o Série 3 para as pistas do campeonato de turismo Grupo A. O que começou como uma exigência de 5.000 unidades de rua transformou-se no sedã esportivo mais icônico do mundo, construindo uma linhagem lendária ao longo de seis gerações.</p>
-    <a href="#" class="btn btn-primary">BMW M3</a>
-  </div>
-</div>
-
-<div class="card" style="width: 18rem;">
-  <img src="/img/img/Skyline R34" class="card-img-top" alt="200">
-  <div class="card-body">
-    <h5 class="card-title">nissan</h5>
-    <p class="card-text">O Nissan Skyline GT-R R34, lançado no Japão em 1999, é um dos maiores ícones automotivos do mundo. Conhecido como "Godzilla", o superesportivo destacou-se pela tração integral ATTESA E-TS e pelo lendário motor biturbo 6 cilindros em linha \(RB26DETT\), que entregava \(280\) cavalos de potência.</p>
-    <a href="#" class="btn btn-primary">Nissan Skyline GT-R R34</a>
-  </div>
-</div>
-
-
-
     <div class="col">Column
          <div class="card" style="width: 18rem;">
          <img src="/img/img/bmw m3.jpg" class="card-img-top" alt="200">
@@ -108,5 +65,4 @@
   <?php include "inc-footer.php"?>
 
 </body>
-
 </html>
