30777d949cd7919ebcf4cf302d1bbddd0500bc49
Kaio <kaiomascarenhas241@gmail.com>
2026-06-09 20:13:30 -0300

    restaurando inc-conexao php

--- 03_projeto_intercador/inc-conexao.php
+++ 03_projeto_intercador/inc-conexao.php
@@ -1,6 +1,10 @@
 <?php
 $conexao = mysqli_connect("localhost","root","", "db_carros_projeto_integrador");
+<<<<<<< Updated upstream
 if($conexao){
+=======
+if(!$conexao){
+>>>>>>> Stashed changes
     die("<h3>erro</h3>" . mysqli_connect_error());
 }
 ?>
