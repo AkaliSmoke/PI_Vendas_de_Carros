fd55e83184362db0bfb215f178f9384248b731e2
leandropro12 <227642795+lelepepe50-droid@users.noreply.github.com>
2026-06-09 21:04:32 -0300

    refeito

--- vendedor-salvar.php
+++ vendedor-salvar.php
@@ -1,19 +1,20 @@
 <?php
+include "inc-conexao.php";
+
 $nome = $_POST["nome"];
 $cpf = $_POST["cpf"];
 $email = $_POST["email"];
 $telefone = $_POST["telefone"];
 $senha = $_POST["senha"];
 
+$sql = "insert into tb_vendedor(nome, cpf, email, telefone, senha) values ('$nome', '$cpf', '$email', '$telefone', '$senha')";
+$resultado = mysqli_query($conexao, $sql);
+
 if($resultado){
     echo "cadastrado com sucesso";
 }else{
     echo "deu algum problema";
 }
- 
-include "inc-conexao.php";
-$sql = "insert into tb_vendedor(nome, cpf, email, telefone, senha) values ('$nome', '$cpf', '$email', '$telefone', '$senha')";
-$resultado = mysqli_query($conexao, $sql);
 
 mysqli_close($conexao);
 ?>
